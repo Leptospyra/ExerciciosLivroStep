ec43e374e43cae7d48b65a4b10a8000f051ededb
Rafael <ferreira.rfl@gmail.com>
2018-05-26 13:06:58 -0300

    initial commit

---
+++ src/ProgramProject.java
@@ -0,0 +1,28 @@
+public class ProgramProject {
+
+
+        String refrao = "";
+        public static void main(String[] args){
+
+        }
+        public static void Refrao(){
+
+            Linha1();
+
+        }
+        public static void Linha1(){
+            System.out.println ("On the first day of Christmas");
+            System.out.println("my true love sent to me");
+            Refrao();
+        }
+        public static void Linha2(){
+            System.out.println ("On the second day of Christmas,");
+            System.out.println("my true love sent to me");
+            Refrao();
+        }
+        public static void Linha3(){
+            System.out.println ("|United| |United|");
+            System.out.println ("|States| |States|");
+        }
+    }
+
